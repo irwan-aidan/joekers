ac51dcda368b48e78fe91a801c819e5ca5f77512
irwan-aidan <91520148+irwan-aidan@users.noreply.github.com>
2022-01-12 00:14:24 +0800

    Add files via upload

---
+++ plugins/update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+	echo "You need to run this script as root"; exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+	echo "OpenVZ is not supported"; exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.co);
+echo "Start Update"
+
+# update
+apt-get install ruby
+gem install lolcat
+apt-get install figlet
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/menu.sh"
+wget -O ssh "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/update/ssh.sh"
+wget -O ssssr "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/update/ssssr.sh"
+wget -O trojaan "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/update/trojaan.sh"
+wget -O v2raay "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/update/v2raay.sh"
+wget -O wgr "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/update/wgr.sh"
+wget -O vless "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/update/vleess.sh"
+wget -O bbr "https://raw.githubusercontent.com/cybertize/joekers/default/plugins/update/bbr.sh"
+chmod +x menu
+chmod +x l2tp
+chmod +x ssh
+chmod +x ssssr
+chmod +x sstpp
+chmod +x trojaan
+chmod +x v2raay
+chmod +x wgr
+chmod +x bbr
+
+echo "0 5 * * * root clear-log && reboot" > /etc/crontab
+echo "0 0 * * * root xp" > /etc/crontab
+echo " Fix minor Bugs"
+echo " Now You Can Change Port Of Some Services"
+echo " Reboot 5 Sec"
+sleep 5
+rm -f update.sh
+reboot
